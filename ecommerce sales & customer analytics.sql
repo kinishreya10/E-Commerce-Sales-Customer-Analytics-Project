b6a2bb4c17fdd42ebc42a67de1a531fa0973efc9
Shreya Kini <kinishreya10@gmail.com>
2026-06-30 16:42:30 +0530

    Update ecommerce sales & customer analytics.sql

--- ecommerce sales & customer analytics.sql
+++ ecommerce sales & customer analytics.sql
@@ -6,8 +6,8 @@ SELECT * FROM orders LIMIT 5;
 SELECT * FROM payments LIMIT 5;
 SELECT * FROM reviews LIMIT 5;
 
--- 2. Schema Validation --
-SELECT column_name, data_type FROM information_schema.columns
+-- 2. Schema Validation 
+SELECT column_name, data_type FROM information_schema.columns 
 WHERE table_name='customers';
 
 SELECT column_name, data_type FROM information_schema.columns
@@ -20,97 +20,104 @@ SELECT column_name, data_type FROM information_schema.columns
 WHERE table_name='reviews';
 
 -- 3. Data Quality Validation --
-SELECT order_id, COUNT(*) as duplicate_count FROM orders
+-- (check if PK is clean (customer_id & order_id)
+SELECT order_id, 
+COUNT(*) as duplicate_count FROM orders
 GROUP BY order_id
 HAVING COUNT(*) > 1;
 
---Check for missing(null) values 
--- Validates referential integrity between payments and orders.
+--Check for missing(null) values if 0 then ok (for customers & orders)
 SELECT COUNT(*) as missing_values FROM orders
 WHERE order_id IS NULL 
    OR order_id = '';
 
-SELECT COUNT(*) as missing_values
-FROM customers
+SELECT COUNT(*) as missing_values FROM customers
 WHERE customer_id IS NULL 
    OR customer_id = '';
    
 -- 1) Check if FK: orders.customer_id → customers.customer_id is valid
-SELECT COUNT(*) AS invalid_customers FROM orders o
+SELECT COUNT(*) AS invalid_customers
+FROM orders o
 LEFT JOIN customers c
 ON o.customer_id = c.customer_id
 WHERE c.customer_id IS NULL;
 
 --2) Check if FK: payments.order_id → orders.order_id is valid
-SELECT COUNT(*) AS invalid_orders FROM payments p
+SELECT COUNT(*) AS invalid_orders
+FROM payments p
 LEFT JOIN orders o
 ON p.order_id = o.order_id
 WHERE o.order_id IS NULL;
 
-SELECT COUNT(*) AS invalid_customers FROM orders o
-LEFT JOIN customers c
-ON o.customer_id = c.customer_id
-WHERE c.customer_id IS NULL;
-
 -- 3) Check Null Values(customers)
-SELECT COUNT(*) total_rows, COUNT(customer_id) customer_id,
-COUNT(customer_city) customer_city, COUNT(customer_state) customer_state
+SELECT
+COUNT(*) total_rows,
+COUNT(customer_id) customer_id,
+COUNT(customer_city) customer_city,
+COUNT(customer_state) customer_state
 FROM customers;
 
 -- Check Null Values(orders)
-SELECT COUNT(*) total_rows,
-COUNT(order_id) order_id, COUNT(customer_id) customer_id,
-COUNT(order_status) order_status, COUNT(order_purchase_timestamp) purchase_time
+SELECT
+COUNT(*) total_rows,
+COUNT(order_id) order_id,
+COUNT(customer_id) customer_id,
+COUNT(order_status) order_status,
+COUNT(order_purchase_timestamp) purchase_time
 FROM orders;
 
 -- Check Null Values(payments)
-SELECT COUNT(*) total_rows,
-COUNT(order_id) order_id, COUNT(payment_type) payment_type,
+SELECT
+COUNT(*) total_rows,
+COUNT(order_id) order_id,
+COUNT(payment_type) payment_type,
 COUNT(payment_value) payment_value
 FROM payments;
 
 -- Check Null Values(reviews)
-SELECT COUNT(*) total_rows,
-COUNT(order_id) order_id, COUNT(review_score) review_score
+SELECT
+COUNT(*) total_rows,
+COUNT(order_id) order_id,
+COUNT(review_score) review_score
 FROM reviews;
 
 -- 4. Relationship Validation --
-SELECT COUNT(*) as total_orders 
+SELECT COUNT(*) as total_orders
 FROM orders;
 
-SELECT COUNT(*) as orders_after_customer_join from orders o
+SELECT COUNT(*) as orders_after_customer_join 
+FROM orders o
 LEFT JOIN customers c
 ON o.customer_id = c.customer_id;
 
-SELECT COUNT(*) as orders_after_payment_join from orders o
+SELECT COUNT(*) as orders_after_payment_join
+from orders o
 LEFT JOIN payments p
 ON o.order_id = p.order_id;
 
--- 5. Create Analytical Master Table --
-CREATE TABLE ecommerce_master1 AS
+-- 1. Create Analytical Master Table --
+CREATE TABLE ecommerce_master1 AS 
 SELECT
     o.order_id,
-    o.customer_id,
-
+    o.customer_id, 
     c.customer_city,
     c.customer_state,
-
-    o.order_status,
-
-	  EXTRACT(YEAR FROM o.order_purchase_timestamp::timestamp) AS purchase_year,
-	  EXTRACT(MONTH FROM o.order_purchase_timestamp::timestamp) AS purchase_month,
-
+    o.order_status, 
+	  EXTRACT(YEAR FROM o.order_purchase_timestamp::timestamp) AS purchase_year, 
+	  EXTRACT(MONTH FROM o.order_purchase_timestamp::timestamp) AS purchase_month, 
+	
     -- Delivery Days
     (o.order_delivered_customer_date::date -
      o.order_purchase_timestamp::date) AS delivery_days,
-
-    -- Delay Days
+	 
+    -- Delivery Delay Days
     GREATEST(
     (
         o.order_delivered_customer_date::timestamp::date -
         o.order_estimated_delivery_date::timestamp::date
-    ), 0) AS delivery_delay_days,
-
+    ),
+    0
+) AS delivery_delay_days,
     -- Delayed Flag
     CASE
         WHEN o.order_delivered_customer_date::timestamp >
@@ -118,10 +125,9 @@ SELECT
         THEN 1
         ELSE 0
 	END AS is_delayed,
-    p.payment_type,
+    p.payment_type, 
     p.payment_value,
-    r.review_score
-  
+    r.review_score 
 FROM orders o
 LEFT JOIN customers c
 ON o.customer_id = c.customer_id
@@ -133,17 +139,17 @@ ON o.order_id = r.order_id;
 select * from ecommerce_master1
 
 -- 6. Business Questions (queries- sql analysis) --
-select * from ecommerce_master1
--- 1) Total Revenue
-select round(sum(payment_value)::numeric,2) as total_revenue
-from payments;
+-- 1) Total Revenue 
+select 
+round(sum(payment_value)::numeric,2) as total_revenue
+from payments; 
 
 -- 2) Total orders
 select count(*) as total_orders
 from orders;
 
 --3) Revenue by payment type
-select payment_type, 
+select payment_type,
 round(sum(payment_value)::numeric,2) revenue
 from payments
 group by payment_type
@@ -159,7 +165,7 @@ order by purchase_year, purchase_month asc;
 --5) Delayed orders
 select 
 round(100.0 * sum(is_delayed) / count(*),2) as delayed_percentage
-from orders;
+from ecommerce_master1;
 
 --6) Average Delivery Time
 select
@@ -168,47 +174,63 @@ FROM orders;
 
 --7) Starts with Highest Revenue
 SELECT customer_state,
-ROUND(SUM(payment_value)::numeric,2) as revenue FROM ecommerce_master1
+ROUND(SUM(payment_value)::numeric,2) as revenue
+FROM ecommerce_master1
 GROUP BY customer_state
 ORDER BY revenue DESC;
 
 --8) Cities with Most Orders
-SELECT customer_city, COUNT(order_id) total_orders FROM ecommerce_master1
+SELECT customer_city,
+COUNT(order_id) total_orders
+FROM ecommerce_master1
 GROUP BY customer_city
 ORDER BY total_orders DESC
 LIMIT 10;
 
 --9) Review Score Distribution
-SELECT review_score, COUNT(*) reviews FROM reviews
+SELECT review_score,
+COUNT(*) reviews
+FROM reviews
 GROUP BY review_score
 ORDER BY review_score;
 
 -- 10) Revenue vs Review Score
 SELECT review_score,
-ROUND(SUM(payment_value)::numeric,2) as revenue FROM ecommerce_master1
+ROUND(SUM(payment_value)::numeric,2) as revenue
+FROM ecommerce_master1
 GROUP BY review_score
 ORDER BY review_score;
 
 -- 3. Advanced SQL Queries -- 
 -- 1) Top 10 Customers by Spending
-SELECT customer_id,
-ROUND(SUM(payment_value)::numeric,2) total_spent FROM ecommerce_master1
+SELECT
+customer_id,
+ROUND(SUM(payment_value)::numeric,2) total_spent
+FROM ecommerce_master1
 GROUP BY customer_id
 ORDER BY total_spent DESC
 LIMIT 10;
 
 --2) Customer Segmentation
+-- This query divides customers into groups based on how much they spend
 SELECT customer_id,
 CASE
 WHEN SUM(payment_value) > 1000 THEN 'VIP'
 WHEN SUM(payment_value) > 500 THEN 'Regular'
-ELSE 'Low Value' END customer_segment FROM ecommerce_master1
+ELSE 'Low Value' END customer_segment
+FROM ecommerce_master1
 GROUP BY customer_id;
 
 --3) Monthly Growth Rate
-WITH monthly_sales AS (SELECT purchase_year, purchase_month,
-SUM(payment_value) as revenue FROM ecommerce_master1
+WITH monthly_sales AS (
+SELECT purchase_year, purchase_month,
+SUM(payment_value)::numeric as revenue
+FROM ecommerce_master1
 GROUP BY purchase_year, purchase_month)
-SELECT *, LAG(revenue) OVER() previous_month,
-ROUND(((revenue - LAG(revenue) OVER()) / LAG(revenue) OVER()*100)::numeric,2)
-AS growth_percent FROM monthly_sales;
+
+SELECT *, -- Main Query
+LAG(revenue) OVER(order by purchase_year, purchase_month) as previous_month, 
+ROUND(((revenue - LAG(revenue) OVER(order by purchase_year, purchase_month)) 
+/ LAG(revenue) OVER(order by purchase_year, purchase_month)*100)::numeric,2)
+AS growth_percent
+FROM monthly_sales;
